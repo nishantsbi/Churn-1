be3f511f4041393973db57cdb6175a2573fc3a16
rkmiec1234 <mr.kmieciak@gmail.com>
2014-09-26 16:31:57 -0700

    asdf

--- scratch.R
+++ scratch.R
@@ -1,16 +1,18 @@
 source("home.R")
 table <- tbl(stat_store,"account")
 table.sflTrialStarts <- table %>%
-  filter(!is.na(firstSignInClient),!is.na(trialFromDate),trialFromDate>"2014-09-01") %>%
-  mutate(converted = !is.na(fromDate)) %>%
-  select(trialFromDate,fromDate,channel,firstSignInClient,converted) %>%
-  group_by(trialFromDate,firstSignInClient) %>%
+  filter(!is.na(firstSignInClient),!is.na(trialFromDate),trialFromDate>"2014-01-01") %>%
+  mutate(converted = !is.na(fromDate),trialFromMonth = month(trialFromDate)) %>%
+  select(trialFromDate,trialFromMonth, fromDate,channel,firstSignInClient,converted) %>%
+  group_by(trialFromMonth,firstSignInClient) %>%
   summarise(count = n(),
             conversions = sum(converted),
-            rate = sum(converted)/n())
+            rate = sum(converted)/n()) %>%
+
+
             
-  
 print(table.sflTrialStarts)
 print(table)
 
-
+rm(table)
+rm(table.sflTrialStarts)
