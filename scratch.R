ad509444aeb8b9a362cb2ee9776aade6ab81f94b
rkmiec1234 <mr.kmieciak@gmail.com>
2014-09-26 16:11:39 -0700

    developing first sign-in client report

--- scratch.R
+++ scratch.R
@@ -1,3 +1,12 @@
 source("home.R")
 table <- tbl(stat_store,"account")
+table.sflTrialStarts <- table %>%
+  filter(!is.na(firstSignInClient),!is.na(trialFromDate),trialFromDate>"2014-09-01") %>%
+  select(trialFromDate,fromDate,channel,firstSignInClient) %>%
+  group_by(trialFromDate,firstSignInClient) %>%
+  summarise(count = n())
+  
+print(table.sflTrialStarts)
+print(table)
+
 
