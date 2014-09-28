657510816227b00c07b66f55e324919351fcb6fc
Richard Kmieciak <mr.kmieciak@gmail.com>
2014-09-28 15:19:33 -0700

    conversion rate refactor

--- scratch.R
+++ scratch.R
@@ -1,9 +1,8 @@
 source("home.R")
 table <- tbl(stat_store,"account")
 table.sflTrialStarts <- table %>%
-  filter(!is.na(firstSignInClient),!is.na(trialFromDate),trialFromDate>"2014-01-01") %>%
-  mutate(converted = !is.na(fromDate),trialFromMonth = month(trialFromDate)) %>%
-  select(trialFromDate,trialFromMonth, fromDate,channel,firstSignInClient,converted) %>%
+  filter(!is.na(firstSignInClient),!is.na(trialFromDate),date(trialFromDate)>="2014-01-01") %>%
+  mutate(converted = !is.na(fromDate)) %>%
   group_by(trialFromMonth,firstSignInClient) %>%
   summarise(count = n(),
             conversions = sum(converted),
--- scratchHome.R
+++ scratchHome.R
@@ -1,1 +1,9 @@
 source("homehome.R")
+t <- tbl(stat_store,"account")
+t.trialConversionRate <- t %>%
+mutate(dateTrialFromDate = date(trialFromDate)) %>%
+  group_by(dateTrialFromDate) %>%
+  summarise(rate = sum(!is.na(fromDate))/n())
+df <- collect(t.trialConversionRate)
+ggvis(df,~dateTrialFromDate,~rate) %>%
+  layer_lines()
