06a232e47040845b3abf98b6924ba4b02dfc2d78
rkmiec1234 <mr.kmieciak@gmail.com>
2014-09-29 09:01:09 -0700

    fixed bugs, now plotting ggplot, ggvis still broken - won't draw line

--- scratch.R
+++ scratch.R
@@ -2,14 +2,18 @@ source("home.R")
 t <- tbl(stat_store,"account")
 t.trialConversionRate <- t %>%
   mutate(dateTrialFromDate = date(trialFromDate)) %>%
-  group_by(dateTrialFromDate,firstSignInClient) %>%
+  filter(trialFromDate > "2014-08-27") %>%
+  group_by(dateTrialFromDate) %>%
   summarise(rate = sum(!is.na(fromDate))/n())
+
 df <- collect(t.trialConversionRate)
 df$month <- month(df$dateTrialFromDate)
+df$dateTrialFromDate <- ymd(df$dateTrialFromDate)
 
-ggvis(df,~month,~rate,stroke=~firstSignInClient) %>%
-  layer_lines()
+ggplot(df,aes(x=dateTrialFromDate,y=rate)) + geom_area(alpha=.2)
+df %>% ggvis(~dateTrialFromDate,~rate) %>% layer_points() %>% layer_lines()
 
 rm(t)
 rm(t.trialConversionRate)
 rm(df)
+
