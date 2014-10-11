a37467853818010ab517818b7df2501007f81361
rkmiec1234 <mr.kmieciak@gmail.com>
2014-10-10 19:54:34 -0700

    asdf

--- scratch.R
+++ scratch.R
@@ -2,15 +2,29 @@ source("http://rako1.com/gaHome.R")
 source("http://rako1.com/home.R")
 source("http://rako1.com/utility.R")
 
-head(df.paid)
-##beginning of period
+
+df.paid <- read.csv("sflPaid1010.csv")
+df.paid$date <- mdy(df.paid$date)
+df.churnRateByMonth <- df.paid %>%
+  group_by(month(date)) %>%
+  mutate(lastDay = ceiling_date(ymd("2014-01-23"),unit = "month") - days(1),
+         firstDay = floor_date(ymd("2014-01-23"),unit = "month"))
+head(df.churnRateByMonth)
+
+ceiling_date(ymd("2014-01-23"),unit = "month") - days(1)
+
+
+
+##group_by yearmon (WORKING)
 df.paid <- read.csv("sflPaid1010.csv")
 df.paid$date <- mdy(df.paid$date)
 df.group <- df.paid %>%
   mutate(month = as.numeric(as.yearmon(date))) %>%
   group_by(month)%>%
-  mutate(month = as.yearmon(month))
-headf.group
+  mutate(ym = as.yearmon(month)) %>%
+  ungroup() %>%
+  select(ym,paidSub,churned)
+head(df.group)
 
 
 
